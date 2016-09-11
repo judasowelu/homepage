0157c086446b1b81679b888fe2bae54b689b6507
judasowelu <judasowelu@gmail.com>
2016-09-11 23:41:51 +0900

    업로드 이미지 용량 조절

--- src/public/js/page.js
+++ src/public/js/page.js
@@ -63,8 +63,38 @@ var page = new function () {
 		if ( input.files && input.files[0] ) {
 			var FR= new FileReader();
 			FR.onload = function(e) {
-				var img = $("<span class='image fit'><img src='"+e.target.result+"' /></span>");
-				$('#content ').append(img)
+				var $image = $("<img>");
+				$image.attr("src", e.target.result);
+				var img = $image[0];
+				
+				var canvas = document.createElement('canvas');
+				
+				var MAX_WIDTH = 960;
+				var MAX_HEIGHT = 960;
+				var width = img.width;
+				var height = img.height;
+				 
+				if (width > height) {
+				  if (width > MAX_WIDTH) {
+				    height *= MAX_WIDTH / width;
+				    width = MAX_WIDTH;
+				  }
+				} else {
+				  if (height > MAX_HEIGHT) {
+				    width *= MAX_HEIGHT / height;
+				    height = MAX_HEIGHT;
+				  }
+				}
+				canvas.width = width;
+				canvas.height = height;
+				var ctx = canvas.getContext("2d");
+				ctx.drawImage(img, 0, 0, width, height);
+
+//				$image.attr("src", canvas.toDataURL("image/png"));
+				var $span = $("<span class='image fit'>");
+				$span.append($image)
+
+				$('#content ').append($span)
 			};
 			FR.readAsDataURL( input.files[0] );
 		}
