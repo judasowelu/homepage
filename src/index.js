774c3effa7b642cbfab781885ea4662bda206c24
judasowelu <judasowelu@gmail.com>
2016-09-14 03:29:37 +0900

    모듈추가

---
+++ src/index.js
@@ -0,0 +1,75 @@
+var homeurl = document.getElementsByTagName('script')[0].getAttribute("src").replace("/i", "");
+function scriptTag(src, callback) {
+	if (typeof callback == "undefined") {
+		callback = {};
+	}
+    var s = document.createElement('script');
+    s.type = 'text/' + (src.type || 'javascript');
+    s.src = src.src || src;
+    s.async = false;
+
+    s.onreadystatechange = s.onload = function () {
+        var state = s.readyState;
+        if (!callback.done && (!state || /loaded|complete/.test(state))) {
+            callback.done = true;
+            if (typeof callback === "function") {
+            	callback();
+            }
+        }
+    };
+
+    (document.head || document.body).appendChild(s);
+}
+
+scriptTag(homeurl+"/public/js/jquery/jquery-2.1.1.min.js", function () {
+	scriptTag(homeurl+"/socket.io/socket.io.js", initSocket);
+});
+
+var socket;
+function initSocket () {
+	socket = io.connect(homeurl);
+	socket.on('head append', function(m) {
+		$("head").append(m);
+		socket.emit("hash", window.location.hash.substr(1));
+	});
+	
+	socket.on('script append', function(m) {
+		loadjscssfile(homeurl + "/assets/js/" + m, "js")
+	});
+	
+	socket.on('body', function(m) {
+		$("body").append(m);
+		$("#nav a").removeClass("active");
+		$("#nav [href='#"+window.location.hash.substr(1)+"']").addClass("active");
+	});
+	
+	socket.on('wrapper', function(m) {
+		$("#wrapper").prepend(m);
+	});
+
+	socket.on('ready', function (data) {
+		var path = window.location.hash.substr(1);
+		if (path.indexOf(".page") > 0) {
+			socket.emit('page data', {pageId : path.replace(".page", "")});
+		} else {
+			socket.emit('page data', data);
+		}
+
+	});
+	
+	socket.on('page data', function (data) {
+		page.load(data);
+	});
+	
+	socket.on('done add sub page', function (data) {
+		requestStorage ();
+	});
+	
+	socket.on('done remove sub page', function (data) {
+		requestStorage ();
+	});
+	
+	socket.on('done save page', function (data) {
+		requestStorage ();
+	});
+}
